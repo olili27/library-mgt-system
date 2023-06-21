09a26f2bd28fc7669b538ec57a3f9e6855f6b513
olili27 <timmolili27@gmail.com>
2023-06-22 01:50:25 +0300

    test find allstudents by name

--- src/test/java/com/example/library/mgt/system/repositories/StudentRepositoryTest.java
+++ src/test/java/com/example/library/mgt/system/repositories/StudentRepositoryTest.java
@@ -2,14 +2,16 @@ package com.example.library.mgt.system.repositories;
 
 import com.example.library.mgt.system.models.Card;
 import com.example.library.mgt.system.models.Student;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
 class StudentRepositoryTest {
@@ -17,44 +19,72 @@ class StudentRepositoryTest {
     @Autowired
     private  StudentRepository studentRepository;
 
-    Student student;
+    Student student1;
+    Student student2;
+    Student student3;
 
     @BeforeEach
     void setUp() {
-        student = Student.builder()
+        student1 = Student.builder()
                 .name("timothy")
                 .age(24)
                 .school("must")
                 .email("timo@gmail.com")
-                .card(Card.builder().student(student).build())
+                .card(Card.builder().student(student1).build())
                 .build();
 
-        studentRepository.save(student);
+        student2 = Student.builder()
+                .name("timothy")
+                .age(26)
+                .school("kyu")
+                .email("timothy@gmail.com")
+                .card(Card.builder().student(student2).build())
+                .build();
+
+        student3 = Student.builder()
+                .name("john")
+                .age(20)
+                .school("kiu")
+                .email("john@gmail.com")
+                .card(Card.builder().student(student1).build())
+                .build();
+
+        studentRepository.saveAll(List.of(student1, student2, student3));
     }
 
     @AfterEach
     void tearDown() {
-        student = null;
+        student1 = null;
+        student2 = null;
+        student3 = null;
         studentRepository.deleteAll();
     }
 
     @Test
     void testFindByEmail_Found() {
         Student studentFromDb = studentRepository.findByEmail("timo@gmail.com");
-        assertThat(studentFromDb.getEmail()).isEqualTo(student.getEmail());
-        assertThat(studentFromDb.getName()).isEqualTo(student.getName());
-        assertThat(studentFromDb.getAge()).isEqualTo(student.getAge());
-        assertThat(studentFromDb.getSchool()).isEqualTo(student.getSchool());
+        assertThat(studentFromDb.getEmail()).isEqualTo(student1.getEmail());
+        assertThat(studentFromDb.getName()).isEqualTo(student1.getName());
+        assertThat(studentFromDb.getAge()).isEqualTo(student1.getAge());
+        assertThat(studentFromDb.getSchool()).isEqualTo(student1.getSchool());
     }
 
     @Test
     void testFindByEmail_NotFound() {
-        Student studentFromDb = studentRepository.findByEmail("timo@gmail.com");
-
+        Student studentFromDb = studentRepository.findByEmail("timo@gmail.com2");
+        assertThat(studentFromDb == null).isTrue();
     }
 
     @Test
-    void testFindAllByName() {
+    void testFindAllByName_Found() {
+        List<Student> studentList = studentRepository.findAllByName("timothy");
+        assertThat(studentList).containsExactlyInAnyOrder(student1, student2);
+    }
+
+    @Test
+    void testFindAllByName_NotFound() {
+        List<Student> studentList = studentRepository.findAllByName("timothy2");
+        assertThat(studentList.isEmpty()).isTrue();
     }
 
     @Test
